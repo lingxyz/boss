64bb36f711c891472d0aa8c82fa363dd29f81fb5
Ling Z <771152260@qq.com>
2021-11-11 10:39:04 +0800

    fix

--- boss.py
+++ boss.py
@@ -29,37 +29,40 @@ listUrl = 'https://www.zhipin.com/wapi/zpjob/rec/geek/list'
 helloUrl = 'https://www.zhipin.com/wapi/zpboss/h5/chat/start?_=1636084604512'
 times = 150 # 设置打招呼次数上限
 limit = { # 设置打招呼的限制条件
-  'intention': 'Java' # 求职意向
+  # 高级后端开发工程师_上海_18-35k: 3279316d461a6b0f1nF52dS0FFRT
+  # nodejs开发工程师: 3860f840951b69af1nF52NS_EVtX
+  # java后端开发工程师: 2b3949330e4897281nF52dS6GFVU
+  "jobId": "2b3949330e4897281nF52dS6GFVU", # 职位id
+  'intention': 'Java', # 求职意向：Java Node.js 前端
+  "major": '809,807', # 专业：计算机、电子信息
+  "experience": '105,106', # 经验：3-5年，5-10年
+  "degree": '203,204', # 学历：本科、硕士
+  "age": "16,-1", # 年龄：16-不限
 }
 
 # 获取候选人列表
 # @param page 当前页数。从1开始
 def getCandidateList(page):
   listData = {
-    "age": "16,-1", # 年龄：16-不限
+    "age": limit['age'],
     "gender": 0,
     "exchangeResumeWithColleague": 0,
     "switchJobFrequency": 0,
     "activation": 0,
     "recentNotView": 0,
     "school": 0,
-    "major": '809,807', # 专业：计算机、电子信息
-    "experience": '105,106', # 经验：3-5年，5-10年
-    "degree": '203,204', # 学历：本科、硕士
+    "major": limit['major'],
+    "experience": limit['experience'],
+    "degree": limit['degree'],
     "salary": 0,
     "intention": 0,
-    # 职位id：
-    # 高级后端开发工程师_上海_18-35k: 3279316d461a6b0f1nF52dS0FFRT
-    # nodejs开发工程师: 3860f840951b69af1nF52NS_EVtX
-    # java后端开发工程师: 2b3949330e4897281nF52dS6GFVU
-    "jobId": "3279316d461a6b0f1nF52dS0FFRT",
+    "jobId": limit['jobId'],
     "page": page
   }
   return requests.get(listUrl, headers=headers, params=listData)
 
 # 候选人打招呼限制：根据条件过滤
 def filterGuy(limit, someGuy):
-  print (limit['intention'] + someGuy['geekCard']['expectPositionName'])
   return limit['intention'] in someGuy['geekCard']['expectPositionName']
 
 # 打招呼
